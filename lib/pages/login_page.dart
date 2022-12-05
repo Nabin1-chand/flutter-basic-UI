71d9ef73a8456902aee622a6d11c386f7fd7e544
Nabin Chand <nbc0117rdx@gmail.com>
2022-12-05 13:12:05 +0545

    animation added on login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,74 +1,102 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("images/login_image.png", fit: BoxFit.cover),
-          SizedBox(
-            height: 20.0,
-          ),
-          // new feauter add in login form
-          Text(
-            "Welcome",
-            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 18.0, horizontal: 40.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter your name",
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter your Email",
-                    labelText: "Email",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter your password",
-                    labelText: "password",
-                  ),
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                Container(
-                  width: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.blue,
-                  ),
-                  child: Center(
-                    child: TextButton(
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(fontSize: 10, color: Colors.white),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("images/login_image.png", fit: BoxFit.cover),
+            SizedBox(
+              height: 0.0,
+            ),
+            // new feauter add in login form
+            Text(
+              "Welcome $name",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 18.0, horizontal: 40.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter your name",
+                        labelText: "Username",
                       ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/home");
-                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      }),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your Email",
+                      labelText: "Email",
                     ),
                   ),
-                )
-              ],
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter your password",
+                      labelText: "password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, "/home");
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 70 : 150,
+                      height: 50,
+                      child: Center(
+                        child: changeButton
+                            ? Icon(Icons.done)
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ), // ElevatedButton(
+                  //   child: Text('login'),
+                  //   style: TextButton.styleFrom(minimumSize: Size(80, 30)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, "/home");
+                  //   },
+                  // )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
